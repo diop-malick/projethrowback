0ceb506afe6bc8d540068860332d1fd676e3be73
Khadre <akgueye.sne@gmail.com>
2017-02-02 17:37:55 +0000

    Shop > afficher nav suivant scroll

--- src/public/throwback16/themes/default-bootstrap-throwback16/css/menu/style.js
+++ src/public/throwback16/themes/default-bootstrap-throwback16/css/menu/style.js
@@ -31,11 +31,13 @@ $(window).scroll(function (event) {
     	$('#block_top_menu').removeClass('show').addClass('hidden');
     	$('.menu_vertical').removeClass('hidden').addClass('show');
     	$('#search_block_top').removeClass('col-sm-4').addClass('col-sm-3');
+    	$('.nav').removeClass('show').addClass('hidden');
     }
     else{
     	$('#block_top_menu').removeClass('hidden').addClass('show');
     	$('.menu_vertical').removeClass('show').addClass('hidden');
     	$('#search_block_top').removeClass('col-sm-3').addClass('col-sm-4');
+    	$('.nav').removeClass('hidden').addClass('show');
 
     	fullPage();
     }
