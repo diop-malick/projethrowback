16348b9dbd9f00c4740131bd2ed0966d80c2faf3
Malick DIOP <mdiop.sne@gmail.com>
2017-02-21 22:38:35 +0000

    shop > post newslitter subscription to mailchimps

--- themes/default-bootstrap-throwback16/js/modules/blocknewsletter/blocknewsletter.js
+++ themes/default-bootstrap-throwback16/js/modules/blocknewsletter/blocknewsletter.js
@@ -24,6 +24,7 @@
 */
 $(document).ready(function() {
 
+    /*
     $('#newsletter-input').on({
         focus: function() {
             if ($(this).val() == placeholder_blocknewsletter || $(this).val() == msg_newsl)
@@ -44,4 +45,94 @@ $(document).ready(function() {
         $('#columns').prepend('<div class="clearfix"></div><p class="' + cssClass + '"> ' + alert_blocknewsletter + '</p>');
 		$('html, body').animate({scrollTop: $('#columns').offset().top}, 'slow');
 	}
+    */
+
+    // mailchimps > form AJX
+    $('#mailchimps-embedded-subscribe-form').submit(function(e) {
+      console.log("mailchimps");
+      var $this = $(this);
+      $.ajax({
+          type: "GET", // GET & url for json slightly different
+          // url: "http://XXXXXXXX.list-manage2.com/subscribe/post-json?c=?",
+          url: "http://ovh.us15.list-manage.com/subscribe/post-json?u=d56def1fdca3f61da060d5d2a&amp;id=2eca580371",
+          data: $this.serialize(),
+          dataType    : 'jsonp',
+          jsonp       : 'c',
+          contentType: "application/json; charset=utf-8",
+          error       : function(err) { alert("Could not connect to the registration server."); },
+          success     : function(data) {
+                mce_success_cb(data);
+            }
+      });
+      return false;
+    });
+
 });
+
+// mailchimps > traitement message d'erreur
+function mce_success_cb(resp){
+    $('#mce-success-response').hide();
+    $('#mce-error-response').hide();
+    if (resp.result=="success"){
+        $('#mce-'+resp.result+'-response').show();
+        $('#mce-'+resp.result+'-response').html(resp.msg);
+        $('#mc-embedded-subscribe-form').each(function(){
+            this.reset();
+        });
+    } else {
+        var index = -1;
+        var msg;
+        try {
+            var parts = resp.msg.split(' - ',2);
+            if (parts[1]==undefined){
+                msg = resp.msg;
+            } else {
+                i = parseInt(parts[0]);
+                if (i.toString() == parts[0]){
+                    index = parts[0];
+                    msg = parts[1];
+                } else {
+                    index = -1;
+                    msg = resp.msg;
+                }
+            }
+        } catch(e){
+            index = -1;
+            msg = resp.msg;
+        }
+        try{
+            if (index== -1){
+                $('#mce-'+resp.result+'-response').show();
+                $('#mce-'+resp.result+'-response').html(msg);            
+            } else {
+                err_id = 'mce_tmp_error_msg';
+                html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
+                
+                var input_id = '#mc_embed_signup';
+                var f = $(input_id);
+                if (ftypes[index]=='address'){
+                    input_id = '#mce-'+fnames[index]+'-addr1';
+                    f = $(input_id).parent().parent().get(0);
+                } else if (ftypes[index]=='date'){
+                    input_id = '#mce-'+fnames[index]+'-month';
+                    f = $(input_id).parent().parent().get(0);
+                } else {
+                    input_id = '#mce-'+fnames[index];
+                    f = $().parent(input_id).get(0);
+                }
+                if (f){
+                    $(f).append(html);
+                    $(input_id).focus();
+                } else {
+                    $('#mce-'+resp.result+'-response').show();
+                    $('#mce-'+resp.result+'-response').html(msg);
+                }
+            }
+        } catch(e){
+            $('#mce-'+resp.result+'-response').show();
+            $('#mce-'+resp.result+'-response').html(msg);
+        }
+    }
+}
+
+
--- themes/default-bootstrap-throwback16/modules/blocknewsletter/blocknewsletter.tpl
+++ themes/default-bootstrap-throwback16/modules/blocknewsletter/blocknewsletter.tpl
@@ -7,17 +7,22 @@
 <div class="col-md-4">	
 
 	<div class="row">
+		
 		<!-- Block Newsletter module-->
 		<div class="col-md-8">
-
 			<div id="newsletter_block_left" class="row block inline">
 				<div class="block_content">
-					<form class="form-inline" action="{$link->getPageLink('index', null, null, null, false, null, true)|escape:'html':'UTF-8'}" method="post">
+					<!-- <form class="form-inline" action="{$link->getPageLink('index', null, null, null, false, null, true)|escape:'html':'UTF-8'}" method="post"> -->
+					<form id="mailchimps-embedded-subscribe-form">
+
 						<div class="news-footer">
 							<i class="fa fa-envelope"></i>
-							<input class="inputNew newsletter-input form-control" id="newsletter-input" type="text" name="email" size="18" value="{if isset($msg) && $msg}{$msg}{elseif isset($value) && $value}{$value}{else}{/if}" placeholder="{l s=' Newsletter ' mod='blocknewsletter'}"/>
+							<input class="inputNew newsletter-input form-control required email" id="newsletter-input" type="text" name="EMAIL" size="18" value="{if isset($msg) && $msg}{$msg}{elseif isset($value) && $value}{$value}{else}{/if}" placeholder="{l s=' Newsletter ' mod='blocknewsletter'}"/>
 							<span class="glyphicon glyphicon-remove-circle"></span>
 							<input type="submit" value="OK" class="btn button-default" name="submitNewsletter" />
+							<br>
+							<span class="response" id="mce-error-response" style="display:none"></span>
+							<span class="response" id="mce-success-response" style="display:none"></span>
 							<input type="hidden" name="action" value="0"/>
 						</div>
 					</form>
@@ -26,6 +31,7 @@
 			</div>
 		</div>
 		<!-- /Block Newsletter module-->
+
 {strip}
 {if isset($msg) && $msg}
 {addJsDef msg_newsl=$msg|@addcslashes:'\''}
