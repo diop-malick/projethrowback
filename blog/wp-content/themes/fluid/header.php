0ed06a24e2c4360e483ee08cb59fb723a24e7389
Malick DIOP <mdiop.sne@gmail.com>
2017-02-23 23:22:16 +0000

    blog > theme update

--- blog/wp-content/themes/fluid/header.php
+++ blog/wp-content/themes/fluid/header.php
@@ -36,39 +36,6 @@ $controllerPrestashop->displayHeader();
 
 <body <?php body_class(); cryout_schema_microdata( 'body' );?>>
 	<?php cryout_body_hook(); ?>
-
-	<header id="masthead" class="cryout" <?php cryout_schema_microdata( 'header' ) ?> role="banner">
-
-		<div id="site-header-main">
-			<div id="site-header-main-inside">
-
-				<nav id="mobile-menu">
-					<span id="nav-cancel"><i class="blicon-cross3"></i></span>
-					<?php cryout_mobilemenu_hook(); ?>
-				</nav> <!-- #mobile-menu -->
-
-				<div id="branding">
-					<?php cryout_branding_hook();?>
-				</div><!-- #branding -->
-
-				<?php cryout_header_socials_hook();?>
-
-				<a id="nav-toggle"><span>&nbsp;</span></a>
-				<nav id="access" role="navigation"  aria-label="Primary Menu" <?php cryout_schema_microdata( 'menu' ); ?>>
-					<?php cryout_access_hook();?>
-				</nav><!-- #access -->
-
-			</div><!-- #site-header-main-inside -->
-		</div><!-- #site-header-main -->
-
-		<div id="header-image-main">
-			<div id="header-image-main-inside">
-				<?php cryout_headerimage_hook(); ?>
-			</div><!-- #header-image-main-inside -->
-		</div><!-- #header-image-main -->
-
-	</header><!-- #masthead -->
-
 	<?php cryout_breadcrumbs_hook();?>
 
 	<div id="content" class="cryout">
