11c78afac3a062cb0a91fdd577bb90be0554b120
unknown <bdia.sne@gmail.com>
2017-02-23 16:59:38 +0000

    shop > zone de recherche > vider le champ de recherche

--- override/classes/controller/FrontController.php
+++ override/classes/controller/FrontController.php
@@ -7,7 +7,7 @@ class FrontController extends FrontControllerCore
         $this->addCSS(_THEME_CSS_DIR_.'menu/vertical-responsive-menu.css');
         $this->addCSS(_THEME_CSS_DIR_.'style.css');
         $this->addJS(_THEME_JS_DIR_.'menu/define.js');
-        if (isset($this->context->controller->php_self) && $this->context->controller->php_self == 'index')
+        if (isset($this->context->controller->php_self) && $this->context->controller->php_self == 'index' || isset($this->context->controller->php_self) && $this->context->controller->php_self == 'search')
             $this->addJS(_THEME_JS_DIR_.'menu/home.js');
         elseif (isset($this->context->controller->php_self) && $this->context->controller->php_self == 'category')
             $this->addJS(_THEME_JS_DIR_.'menu/category.js');
