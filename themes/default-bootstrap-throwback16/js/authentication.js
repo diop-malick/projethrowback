e6ec090d971ada4acb8110ea1f6c6ea3f9170c88
Malick DIOP <mdiop.sne@gmail.com>
2017-03-02 21:46:25 +0000

    shop > mailchimps newletter on account creation

--- themes/default-bootstrap-throwback16/js/authentication.js
+++ themes/default-bootstrap-throwback16/js/authentication.js
@@ -1,34 +1,94 @@
-/*
-* 2007-2016 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 $(document).ready(function(){
+
+	var isBusy = false; //first time, busy state is false
+
 	$(document).on('submit', '#create-account_form', function(e){
 		e.preventDefault();
+		// console.log('create-account_form');
 		submitFunction();
 	});
 	$('.is_customer_param').hide();
+
+
+	
+	$(document).on('submit', '#account-creation_formTMP', function(e){
+			e.preventDefault();
+			e.returnValue = false;
+
+			var $form = $(this);			
+				console.log('account-creation_form - Mailchimps');
+				
+				// Find out if a single newletter checkbox is checked or not
+				if ($('#newsletter').is(":checked"))
+				{
+				  console.log('Checked');
+				  var email = $('#email').val();
+			        var customer_firstname = $('#customer_firstname').val();
+			        var customer_lastname = $('#customer_lastname').val();
+			        if(customer_firstname === '' || customer_lastname === '' || email === '') {
+			            console.log('fields are required');
+			        } else {
+			        	    $.ajax({
+					          type: "GET",
+					          url: "https://ovh.us15.list-manage.com/subscribe/post-json?u=d56def1fdca3f61da060d5d2a&amp;id=2eca580371",
+					          data: {
+					          	"EMAIL": email,
+					          	"FNAME": customer_firstname,
+					          	"LNAME": customer_firstname
+					          },
+					          dataType    : 'jsonp',
+					          jsonp       : 'c',
+					          contentType: "application/json; charset=utf-8",
+					          error       : function(err) { alert("Could not connect to the registration server."); },
+					          success     : function(data) {
+					          	console.log(data.msg); 
+					         },
+					         complete: function() { 
+					         	console.log('Complete');
+					            submitCustomFunction();
+					         }
+					      });
+			        }
+				} else {
+					console.log('Not checked');
+					isBusy = true;
+					// e.returnValue = true;
+					//$form.submit();
+				}
+
+			// return true;		  
+	});
+	
 });
+
+function submitCustomFunction()
+{
+		$.ajax({
+			type: 'POST',
+			url: baseUri + '?rand=' + new Date().getTime(),
+			async: true,
+			cache: false,
+			dataType : "json",
+			headers: { "cache-control": "no-cache" },
+			data:
+			{
+				controller: 'authentication',
+				submitAccount: 1,
+				ajax: true,
+				token: token
+			},
+			success: function(jsonData)
+			{
+				console.log('submitCustomFunction OK');
+				console.log(jsonData);
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown)
+			{
+				console.log('submitCustomFunction , not OK');
+			}
+		}
+}
+
 
 function submitFunction()
 {
