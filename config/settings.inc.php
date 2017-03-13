ad0a5b9f9b2b798fba32e89e47471938c47c7ae7
root <root@vps365425.ovh.net>
2017-03-13 10:22:29 +0100

    db settings

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -3,7 +3,7 @@
 define('_DB_SERVER_', 'localhost');
 define('_DB_NAME_', 'throwbackpresta16');
 define('_DB_USER_', 'root');
-define('_DB_PASSWD_', 'root');
+define('_DB_PASSWD_', 'mysqlsne123');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
