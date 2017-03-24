48cb5a57087544304339e323b3244348dd17235e
Malick DIOP <mdiop.sne@gmail.com>
2017-03-24 08:13:56 +0000

    shop > FP > Coming soon > minosr corrections

--- themes/default-bootstrap-throwback16/js/product.js
+++ themes/default-bootstrap-throwback16/js/product.js
@@ -232,15 +232,13 @@ $(document).ready(function()
 	}
 	// console.log('comingsoonvalue =' + comingsoonvalue);
 	// Coming soon disable
-	if ((typeof available_date !== 'undefined' && available_date) || (typeof comingsoonvalue !== 'undefined' && comingsoonvalue)) {
+	if ( ((typeof available_date !== 'undefined') && available_date) || ((typeof comingsoon_value !== 'undefined') && comingsoon_value) ) {
 		$('#quantity_wanted_p .btn').removeClass('active').addClass('disabled');
 		$('#add_to_cart button').removeClass('active').addClass('disabled');
 		$('#color_to_pick_list .btn').removeClass('active').addClass('disabled');
 		$('.attribute_list .btn').removeClass('active').addClass('disabled');
 		$('.attribute_label').removeClass('active').addClass('btn').addClass('disabled');
 		$('#quantity_wanted_p label').removeClass('active').addClass('btn').addClass('disabled');
-
-		
 	}
 	
 
