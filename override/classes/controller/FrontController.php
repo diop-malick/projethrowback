f63e432d2966767e73f7b3114a61c7576f0ca0c2
Khadre <akgueye.sne@gmail.com>
2017-04-27 10:40:29 +0000

    0000186: [Throwback][Galaxy TAB 2][Livraison] Champ téléphone obligatoire lors de l'ajout d'une nouvelle adresse

--- override/classes/controller/FrontController.php
+++ override/classes/controller/FrontController.php
@@ -7,10 +7,10 @@ class FrontController extends FrontControllerCore
         $this->addCSS(_THEME_CSS_DIR_.'menu/vertical-responsive-menu.css');
         $this->addCSS(_THEME_CSS_DIR_.'style.css');
 
-        $this->addJS(_THEME_JS_DIR_.'global_custom.js');
-
         $this->addJS(_THEME_JS_DIR_.'form-validator/jquery.form-validator.min.js');
-        $this->addCSS(_THEME_JS_DIR_.'form-validator/theme-default.css');        
+        $this->addCSS(_THEME_JS_DIR_.'form-validator/theme-default.css');  
+
+        $this->addJS(_THEME_JS_DIR_.'global_custom.js');      
 
         $this->addjqueryPlugin('fancybox');
         $this->addJS(_THEME_JS_DIR_.'spin.js');
