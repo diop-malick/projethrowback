ec8e188947626259a05d7cb32aeef05fde7becb0
Khadre <akgueye.sne@gmail.com>
2017-06-06 14:37:26 +0000

    0000240: [Throwback][DESK][Panier] Masquer le mini panier en page panier

--- themes/default-bootstrap-throwback16/js/modules/blockcart/ajax-cart.js
+++ themes/default-bootstrap-throwback16/js/modules/blockcart/ajax-cart.js
@@ -55,8 +55,10 @@ $(document).ready(function(){
 
 	$("#header .shopping_cart a:first").hover(
 		function(){
-			if (ajaxCart.nb_total_products > 0 || ($('.ajax_cart_quantity').html() != 0 && $('.ajax_cart_quantity').html() != "(0)")  )
+			if (ajaxCart.nb_total_products > 0 || ($('.ajax_cart_quantity').html() != 0 && $('.ajax_cart_quantity').html() != "(0)")  ){
+				if(page_name!="order")
 				$("#header .cart_block").stop(true, true).slideDown(450);
+			}
 		},
 		function(){
 			setTimeout(function(){
