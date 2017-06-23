9564fae1461c48e6566385454ee0795d289d1aa9
throwbac <throwbac@ssh1.60gp.ha.ovh.net>
2017-06-23 03:19:17 +0200

    throwback prod commit

---
+++ config/settings.inc.php
@@ -0,0 +1,17 @@
+<?php
+// sqdsq
+define('_DB_SERVER_', 'throwbacpresta16.mysql.db');
+define('_DB_NAME_', 'throwbacpresta16');
+define('_DB_USER_', 'throwbacpresta16');
+define('_DB_PASSWD_', 'AxI3zpb16');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcached');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', '9peDccH4x2iy2D4u9E1SuPQBXdB7G5A4hfHWd777jAw6jd28sLV6SqOr');
+define('_COOKIE_IV_', 'yNLLN7bZ');
+define('_PS_CREATION_DATE_', '2017-01-29');
+if (!defined('_PS_VERSION_'))
+    define('_PS_VERSION_', '1.6.1.11');
+define('_RIJNDAEL_KEY_', 'nvphOzZfXyA9W7ueQZf1x0ettj5p2rWX');
+define('_RIJNDAEL_IV_', 'MHb4tJr7nCMTUgeVF0SErg==');
