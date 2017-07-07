2ce6539bbb261d042a97d302ae4a2ac337ad96bf
Khadre <akgueye.sne@gmail.com>
2017-07-07 15:43:53 +0000

    blog > multi langue

--- blog/wp-content/themes/fluid/header.php
+++ blog/wp-content/themes/fluid/header.php
@@ -38,7 +38,7 @@ Context::getContext()->smarty->assign(array('meta_title' => html_entity_decode(g
 	<!-- BEGIN - intégration WP-PS  -->
 	<?php $controllerPrestashop->displayHeaderBlog(); ?>
 	<!-- End - intégration WP-PS  -->
-
+<div id="weglot_here"></div>
 
 	<!-- Widget RECHERCHE -->
 	<div id="searchform-header">
