c7ac2e512cc2175bd2f68cfbc46412882233593f
Khadre <akgueye.sne@gmail.com>
2017-08-04 12:53:57 +0000

    style page 404

--- blog/wp-content/themes/fluid/style.js
+++ blog/wp-content/themes/fluid/style.js
@@ -1,25 +1,12 @@
 jQuery( document ).ready( function() {
 	var url =document.URL;
 	var  expr = "/en/";
-	function menuTraduction() {
-		jQuery('.sf-menu li:nth-child(1) a').empty().append('News');
-		jQuery('.sf-menu li:nth-child(2) a').empty().append('Sneakers');
-		jQuery('.sf-menu li:nth-child(3) a').empty().append('Clothes');
-		jQuery('.sf-menu li:nth-child(4) a').empty().append('Accessoires');
-		jQuery('.sf-menu li:nth-child(5) a').empty().append('Ladies');
-		jQuery('.sf-menu li:nth-child(6) a').empty().append('Sales');
-		jQuery('.sf-menu li:nth-child(7) a').empty().append('Brands');
-		jQuery('.sf-menu li:nth-child(8) a').empty().append('Blog');
-	}
+	
 	function SearchFormTraduction() {
 		jQuery('.searchform input[type=search]').attr("placeholder", "Your search");
 		
 	}
-	function footerTraduction() {
-		jQuery('.footer-block .list-inline li:nth-child(1) a').empty().append('Contact us');
-		jQuery('.footer-block .list-inline li:nth-child(2) a').empty().append('Services');
-		jQuery('.footer-block .list-inline li:nth-child(3) a').empty().append('Legal notice');
-	}
+	
 	function linkNavigationTraduction() {
 		jQuery('.accueil a').empty().append('Home');
 		jQuery('.nav-previous a').empty().append('Previous article');
@@ -33,9 +20,7 @@ jQuery( document ).ready( function() {
 	}
 	
 	if(url.match(expr)){
-		/*menuTraduction();*/
 		SearchFormTraduction();
-		/*footerTraduction();*/
 		linkNavigationTraduction();
 		searchResultTraduction();
 	}
