25bb38e240e1c7b44de4d7e68063a14b152baa21
Khadre <akgueye.sne@gmail.com>
2017-09-12 16:03:40 +0000

    center logo footer

--- blog/wp-content/themes/fluid/style.js
+++ blog/wp-content/themes/fluid/style.js
@@ -25,15 +25,23 @@ jQuery( document ).ready( function() {
 		searchResultTraduction();
 	}
 
+	//On Hover - Works on ios
 	jQuery(".lp-box").click(function(){
-    //On Hover - Works on ios
     	jQuery(this).find(".lp-box-content").show();
  	});
 
+	//click on slider show
  	jQuery(".seriousslider-inner .item a").click(function(e){
  		e.preventDefault();
         window.location.href = jQuery(this).attr('href');
  	});
+
+ 	var isIpad = window.matchMedia("(min-width: 768px) and (max-width: 991px)");
+ 	if(isIpad.matches){
+ 		jQuery('.logo_footer').removeClass('col-sm-2').addClass('col-sm-4').css("text-align", "center");
+ 		jQuery('.newsletter_footer').removeClass('col-sm-6').addClass('col-sm-4');
+ 	}
+ 	
 
 }); 
 
--- themes/default-bootstrap-throwback16/modules/blocknewsletter/blocknewsletter.tpl
+++ themes/default-bootstrap-throwback16/modules/blocknewsletter/blocknewsletter.tpl
@@ -1,10 +1,10 @@
 <!-- Footer Logo  -->
-<div class="col-sm-2 col-md-2 col-xs-12">
+<div class="logo_footer col-sm-2 col-md-2 col-xs-12">
     <img src="{if isset($force_ssl) && $force_ssl}{$base_dir_ssl}{else}{$base_dir}{/if}img/logo/logo-footer.png" class="footer-logo img-responsive" alt="Throwback logo"/>
 </div> <!-- // Footer Logo -->
 
 <!-- Newletter fancyBox -->
-<div class="col-sm-6 col-md-5 col-xs-12">	
+<div class="newsletter_footer col-sm-6 col-md-5 col-xs-12">	
 	<div class="row">
 		
 		<div class="col-sm-8 col-md-8">
